5649118bd56f3e22b38369d61f0e2b8825414c31
robercast <dunamis1.16@gmail.com>
2026-02-17 16:58:40 -0800

    add comment section content, imgs, usernames and comments

---
+++ src/components/CommentSection/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { fluid } from "../../styles/mixins";
+
+export const Comments = styled.section``;
+
+export const Title = styled.h3``;
+
+export const CommentsContainer = styled.section``;
+
+// .comments__flex
+export const CommentsContainerInner = styled.div``;
+
+export const CommentsCard = styled.section``;
+
+export const Image = styled.img``;
+
+export const Username = styled.h4``;
+
+export const Text = styled.p``;
