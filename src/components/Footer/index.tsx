1c0a2d0ad86fbc91fa37b9487fbf7c4a07227efa
robercast <dunamis1.16@gmail.com>
2026-02-22 04:44:50 -0800

    add footer headings, text and icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,7 +18,16 @@ import {
 import { footerMenus, socialLinks } from "../../data/footer";
 import LogoImage from "../../assets/Logo-Caras.png";
 
+// Iconos
+import { Facebook, Instagram, Twitter } from "react-feather";
+
 const Footer = () => {
+    const icons = {
+        facebook: Facebook,
+        instagram: Instagram,
+        twitter: Twitter,
+    };
+
     return (
         <FooterContainer>
             <MenusLegal>
@@ -34,9 +43,42 @@ const Footer = () => {
                 {footerMenus.map((menu, index) => (
                     <Menu key={index}>
                         <Title>{menu.title}</Title>
-                        <Nav></Nav> {/* .map inside nav */}
+                        <Nav>
+                            {menu.links.map((link, index) => (
+                                <Link key={index} href={link.href}>
+                                    {link.name}
+                                </Link>
+                            ))}
+                        </Nav>
                     </Menu>
                 ))}
+
+                <DisclaimerSocial>
+                    <Text>
+                        editorial televisa s.a. de c.v. todos los derechos
+                        reservados. tbg - editorial televisa - entertainment -
+                        news
+                    </Text>
+
+                    <SocialMedia>
+                        {socialLinks.map((link) => {
+                            const Icons = icons[link.name];
+
+                            return (
+                                <LinkIcon
+                                    key={link.name}
+                                    target="_blank"
+                                    aria-label={link.name}
+                                    href={link.href}
+                                >
+                                    <Icon>
+                                        <Icons />
+                                    </Icon>
+                                </LinkIcon>
+                            );
+                        })}
+                    </SocialMedia>
+                </DisclaimerSocial>
             </MenusLegal>
         </FooterContainer>
     );
