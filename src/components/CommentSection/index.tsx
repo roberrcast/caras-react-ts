98d5b76ed5a37f2b031fdfd35c547e29add3bf6e
roberrcast <dunamis1.16@gmail.com>
2026-05-22 00:54:00 -0700

    Feat: add username to alt text in comment section

--- src/components/CommentSection/index.tsx
+++ src/components/CommentSection/index.tsx
@@ -1,38 +1,38 @@
 import React from "react";
 import { users } from "../../data/users";
 import {
-    Comments,
-    CommentsContainer,
-    Title,
-    CommentsContainerInner,
-    CommentsCard,
-    Image,
-    Username,
-    Text,
+  Comments,
+  CommentsContainer,
+  Title,
+  CommentsContainerInner,
+  CommentsCard,
+  Image,
+  Username,
+  Text,
 } from "./styles";
 
 const CommentSection = () => {
-    return (
-        <Comments>
-            <Title>Nuestros suscriptores dicen:</Title>
-            <CommentsContainer>
-                <CommentsContainerInner>
-                    {users.map((user, id) => (
-                        <CommentsCard key={id}>
-                            <Image
-                                src={user.userImage}
-                                alt="imagen de usuario"
-                            />
+  return (
+    <Comments>
+      <Title>Nuestros suscriptores dicen:</Title>
+      <CommentsContainer>
+        <CommentsContainerInner>
+          {users.map((user, id) => (
+            <CommentsCard key={id}>
+              <Image
+                src={user.userImage}
+                alt={`Foto de perfil de ${user.username}`}
+              />
 
-                            <Username>{user.username}</Username>
+              <Username>{user.username}</Username>
 
-                            <Text>{user.comment}</Text>
-                        </CommentsCard>
-                    ))}
-                </CommentsContainerInner>
-            </CommentsContainer>
-        </Comments>
-    );
+              <Text>{user.comment}</Text>
+            </CommentsCard>
+          ))}
+        </CommentsContainerInner>
+      </CommentsContainer>
+    </Comments>
+  );
 };
 
 export default CommentSection;
